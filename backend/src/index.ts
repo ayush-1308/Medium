6f57a4c823a79a7f5c04314ccf5f7967489d84db
Ayush <ayushsinghlts123@gmail.com>
2024-05-27 18:44:07 +0530

    Structuring

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, sign, verify } from 'hono/jwt'
+import { userRouter } from './routes/user'
+import { blogRouter } from './routes/blog'
 
 const app = new Hono<{
   Bindings: {
@@ -9,6 +11,9 @@ const app = new Hono<{
     JWT_SECRET: string,
   }
 }>()
+
+app.route("/api/v1/user", userRouter)
+app.route("/api/v1/blog", blogRouter)
 
 app.use('/api/v1/blog/*', async (c, next) => {
   const header = c.req.header("authorization")
@@ -23,69 +28,4 @@ app.use('/api/v1/blog/*', async (c, next) => {
   }
   await next()
 })
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.post('/api/v1/user/signup', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-}).$extends(withAccelerate())
-
-const body = await c.req.json();
-
-const user = await prisma.user.create({
-  data: {
-    email: body.email,
-    password: body.password,
-  },
-})
-
-const token = await sign({ id: user.id}, c.env.JWT_SECRET)
-return c.json(
-  {jwt: token}
-)
-})
-
-app.post('/api/v1/user/signin',async (c) => {
-  const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
-
-	const body = await c.req.json();
-	const user = await prisma.user.findUnique({
-		where: {
-			email: body.email,
-      password: body.password,
-		}
-	});
-
-	if (!user) {
-		c.status(403);
-		return c.json({ error: "user not found" });
-	}
-
-	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-  // @ts-ignore
-  c.res.cookie('token', jwt, { httpOnly: true, secure: true, sameSite: 'Strict' });
-  c.res.json();
-})
-
-app.post('/api/v1/blog', (c) => {
-
-})
-
-app.put('/api/v1/blog', (c) => {
-
-})
-
-app.get('/api/v1/blog/:id', (c) => {
-
-})
-
-app.get('/api/v1/blog/bulk', (c) => {
-
-})
-
 export default app
---
+++ backend/src/routes/blog.ts
@@ -0,0 +1,19 @@
+import { Hono } from "hono";
+
+export const blogRouter = new Hono()
+
+blogRouter.post('/', (c) => {
+
+})
+
+blogRouter.put('/', (c) => {
+
+})
+
+blogRouter.get('/:id', (c) => {
+
+})
+
+blogRouter.get('/bulk', (c) => {
+
+})

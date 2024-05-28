e7d483809fc3d4df6d8543df0b8d5c542e85438f
Ayush <ayushsinghlts123@gmail.com>
2024-05-28 20:11:51 +0530

    Auth.tsx

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,22 @@
+export const Quote = () => {
+  return (
+    <div className='bg-slate-200 h-screen flex justify-center flex-col'>
+      <div className='flex justify-center'>
+        <div className='max-w-lg'>
+          <div className='text-3xl font-bold'>
+            "The only way to do great work is to love what you do. If you
+            haven't found it yet, keep looking. Don't settle. As with all
+            matters of the heart, you'll know when you find it."
+          </div>
+
+          <div className='max-w-md text-xl font-semibold mt-3'>
+            - Steve Jobs
+          </div>
+          <div className='max-w-md text-sm font-light text-slate-400'>
+            CEO | Acne Corp
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}

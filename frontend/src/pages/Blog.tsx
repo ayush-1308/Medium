45c3f16a91afaa7f26097e6ef0b1111435f7814c
Ayush <ayushsinghlts123@gmail.com>
2024-05-30 18:07:31 +0530

    Complete

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,18 +1,29 @@
-import FullBlog from "../components/FullBlog"
-import { useBlog } from "../hooks"
-import { useParams } from "react-router-dom"
+import { Appbar } from '../components/Appbar'
+import FullBlog from '../components/FullBlog'
+import { Spinner } from '../components/Spinner'
+import { useBlog } from '../hooks'
+import { useParams } from 'react-router-dom'
 
 const Blog = () => {
-  const { id }= useParams()
-  const {loading, blog} = useBlog({
+  const { id } = useParams()
+  const { loading, blog } = useBlog({
     id: id || ''
   })
-  if(loading){
-    return <div>Loading...</div>
+  if (loading) {
+    return (
+      <div>
+        <Appbar />
+        <div className='h-screen flex flex-col justify-center'>
+          <div className='flex justify-center'>
+            <Spinner />
+          </div>
+        </div>
+      </div>
+    )
   }
   return (
     <div>
-      <FullBlog blog={blog}/>
+      {blog && <FullBlog blog={blog} />}
     </div>
   )
 }
--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -18,12 +18,12 @@ export const Publish = () => {
         <textarea
           id='message'
           onChange={(e) => setTitle(e.target.value)}
-          className='block mb-6 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          className='block mb-6 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500'
           placeholder='Title...'
         ></textarea>
         <TextEditor onChange={(e) => setContent(e.target.value)}/>
         <button onClick={async () => {
-        const response = await axios.post(`${BackendUrl}/api/ve/blogs`, {
+        const response = await axios.post(`${BackendUrl}/api/v1/blog`, {
             title,
             content
         },{
@@ -31,7 +31,7 @@ export const Publish = () => {
                 Authorization: localStorage.getItem("jwt")
             }
         })
-        navigate(`/blog/${response.data.blog.id}`)
+        navigate(`/blog/${response.data.id}`)
    }} type="submit" className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
        Publish post
    </button>
@@ -48,7 +48,7 @@ function TextEditor ({onChange} : {onChange: (e: any) => void}){
     <div className="mb-6 bg-white rounded-lg border dark:border-gray-600">
        <div className=" bg-white rounded-b-lg">
            <label htmlFor="editor" className="sr-only">Publish post</label>
-           <textarea onChange={onChange} id="editor" rows={8} className="focus:outline-none block w-full px-0 text-sm text-gray-800 bg-white border-0  focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write an article..." required ></textarea>
+           <textarea onChange={onChange} id="editor" rows={8} className="focus:outline-none block w-full px-0 text-sm text-gray-800 bg-white border-0  focus:ring-0 dark:placeholder-gray-400" placeholder="Write an article..." required ></textarea>
        </div>
    </div>
    
